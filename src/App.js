aac5259a49b58cbefc62d1891feb00e9ab5de55f
sundar sanjay deshmukh <sundardeshmuk01@gmail.com>
2023-07-26 01:55:34 +0530

    xx

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
-function Ganeshaya(){
+export function Ganeshaya(){
     return (
       <div>
       <h1>Om Shree  Ganeshay namaha</h1>
-      <p>ram ram jai raja ram</p>
+      <p>ram  ram jai raja ram</p>
       </div>
     )
   }
+  
